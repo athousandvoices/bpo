fc4ab46fa0c88a9e3a966a4e5c0c6b2898ac7f6a
hjh2979 <10201332@naver.com>
2021-01-27 16:14:09 +0900

    message 1

---
+++ src/main/java/com/example/board/controller/helloController.java
@@ -0,0 +1,44 @@
+package com.example.board.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class helloController {
+
+    private static final String HELLO = "helloworld-nice to meet you";
+
+    @Setter
+    @Getter
+    public static class Hello {
+        private String message;
+    }
+
+    @GetMapping(value = "/helloworld/string")
+    @ResponseBody
+    public String helloworldString() {
+        log.debug("Helloworld");
+        log.info("Helloworld");
+        return HELLO;
+    }
+
+    @GetMapping(value = "/helloworld/json")
+    @ResponseBody
+    public Hello helloworldJson() {
+        Hello hello = new Hello();
+        hello.message = HELLO;
+        return hello;
+    }
+
+    @GetMapping("/helloworld/long-process")
+    @ResponseBody
+    public String pause() throws InterruptedException {
+        Thread.sleep(10000);
+        return "Process finished";
+    }
+}
---
+++ src/main/java/com/example/board/startApp.java
@@ -0,0 +1,12 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class startApp {
+    public static void main(String[] args) {
+        SpringApplication.run(startApp.class, args);
+    }
+            
+}
