870e0224b8fd54d537721482d65402f76267fd11
hjh2979 <10201332@naver.com>
2021-02-01 14:31:12 +0900

    msg

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class startApp {
+public class BoardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(startApp.class, args);
+        SpringApplication.run(BoardApplication.class, args);
     }
-            
 }
